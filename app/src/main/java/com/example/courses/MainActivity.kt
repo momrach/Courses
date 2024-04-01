091c19e240c763f52a1b920aad18b414d91712de
Miguel <mmrach@gmail.com>
2024-04-01 19:06:26 +0200

    Correccion

--- app/src/main/java/com/example/courses/MainActivity.kt
+++ app/src/main/java/com/example/courses/MainActivity.kt
@@ -1,16 +1,26 @@
 package com.example.courses
 
-import android.media.Image
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.material3.*
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -30,7 +40,7 @@ class MainActivity : ComponentActivity() {
             CoursesTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
-                    color = MaterialTheme.colorScheme.background
+                    color = MaterialTheme.colorScheme.inversePrimary
                 ) {
                     TopicGrid(
                         DataSource.topics,
@@ -46,7 +56,7 @@ class MainActivity : ComponentActivity() {
 private fun TopicGrid(topics: List<Topic>,modifier: Modifier = Modifier) {
     LazyVerticalGrid(
         columns = GridCells.Fixed(2),
-        verticalArrangement = Arrangement.spacedBy(8.dp),
+        verticalArrangement = Arrangement.spacedBy(16.dp),
         horizontalArrangement = Arrangement.spacedBy(8.dp),
         modifier = modifier.padding(8.dp)
     ) {
@@ -64,17 +74,15 @@ fun TopicCard(topic: Topic, modifier: Modifier = Modifier) {
 
     ) {
         Row(){
-            Box() {
-                Image(
-                    painter = painterResource(id = topic.imageRes),
-                    contentDescription = null,
-                    modifier = modifier
-                        .fillMaxHeight()
-                        //.size(width = 68.dp, height = 68.dp)
-                        .aspectRatio(1f),
-                    contentScale = ContentScale.Crop
-                )
-            }
+            Image(
+                painter = painterResource(id = topic.imageRes),
+                contentDescription = null,
+                modifier = modifier
+                    .fillMaxHeight()
+                    //.size(width = 68.dp, height = 68.dp)
+                    .aspectRatio(1f),
+                contentScale = ContentScale.Crop
+            )
 
             Column() {
                 Text(
